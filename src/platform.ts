09955a279b28b5495d008fd212d0df2187e4937c
Julien Ott <julien.ott@gmail.com>
2023-11-25 09:45:32 +0100

    first commit

--- src/platform.ts
+++ src/platform.ts
@@ -1,116 +1,120 @@
-import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
+import axios from 'axios';
+import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig } from 'homebridge';
+import { HeatzyAccessory } from './platformAccessory';
 
-import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { ExamplePlatformAccessory } from './platformAccessory';
-
-/**
- * HomebridgePlatform
- * This class is the main constructor for your plugin, this is where you should
- * parse the user config and discover/register accessories with Homebridge.
- */
-export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
-  public readonly Service: typeof Service = this.api.hap.Service;
-  public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
-
-  // this is used to track restored cached accessories
-  public readonly accessories: PlatformAccessory[] = [];
+export class Heatzy implements DynamicPlatformPlugin {
+  private readonly accessories: PlatformAccessory[] = [];
+  private token: string | null = null;
+  private tokenExpireAt: number | null = null;
 
   constructor(
     public readonly log: Logger,
     public readonly config: PlatformConfig,
     public readonly api: API,
   ) {
-    this.log.debug('Finished initializing platform:', this.config.name);
-
-    // When this event is fired it means Homebridge has restored all cached accessories from disk.
-    // Dynamic Platform plugins should only register new accessories after this event was fired,
-    // in order to ensure they weren't added to homebridge already. This event can also be used
-    // to start discovery of new accessories.
-    this.api.on('didFinishLaunching', () => {
-      log.debug('Executed didFinishLaunching callback');
-      // run the method to discover / register your devices as accessories
-      this.discoverDevices();
-    });
+    this.log.info('Heatzy Plugin Finished Launching');
+    this.api.on('didFinishLaunching', () => this.authenticate());
   }
 
-  /**
-   * This function is invoked when homebridge restores cached accessories from disk at startup.
-   * It should be used to setup event handlers for characteristics and update respective values.
-   */
-  configureAccessory(accessory: PlatformAccessory) {
-    this.log.info('Loading accessory from cache:', accessory.displayName);
+  async authenticate() {
+    try {
+      const response = await axios.post('https://euapi.gizwits.com/app/login', {
+        username: this.config.username,
+        password: this.config.password,
+        lang: 'en',
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'X-Gizwits-Application-Id': 'c70a66ff039d41b4a220e198b0fcc8b3',
+        },
+      });
 
-    // add the restored accessory to the accessories cache so we can track if it has already been registered
+      const { token } = response.data;
+      this.token = token;
+      this.tokenExpireAt = new Date().getTime() + 3600000; // 1 hour token validity
+      this.fetchDevices();
+
+    } catch (error) {
+      this.log.error('Error authenticating:', (error as Error).message);
+    }
+  }
+
+  async fetchDevices() {
+    if (!this.token) {
+      this.log.error('Token not available, unable to fetch devices');
+      return;
+    }
+
+    try {
+      const response = await axios.get('https://euapi.gizwits.com/app/bindings?limit=20&skip=0', {
+        headers: {
+          'Accept': 'application/json',
+          'X-Gizwits-User-token': this.token,
+          'X-Gizwits-Application-Id': 'c70a66ff039d41b4a220e198b0fcc8b3',
+        },
+      });
+
+      const devices = response.data.devices;
+      const selectedModes = this.config.modes || [];
+
+      // Remove accessories not present in fetched devices or not in selected modes
+      this.accessories.forEach(accessory => {
+        const isDeviceFetched = devices.some(device => accessory.context.device.did === device.did);
+        const isModeSelected = selectedModes.includes(accessory.context.mode);
+
+        if (!isDeviceFetched || !isModeSelected) {
+          this.log.info('Removing unused accessory:', accessory.displayName);
+          this.api.unregisterPlatformAccessories('homebridge-heatzy-pilote-platform', 'Heatzy', [accessory]);
+        }
+      });
+
+      // Add or update accessories for fetched devices
+      devices.forEach(device => {
+        selectedModes.forEach(mode => {
+          this.addAccessory(device, mode); // Create accessory for each selected mode
+        });
+      });
+
+      this.log.info('Fetched devices:', devices.length);
+    } catch (error) {
+      this.log.error('Error fetching devices:', (error as Error).message);
+    }
+  }
+
+  addAccessory(device: any, mode: string) {
+    const uniqueId = device.did + ' ' + mode; // Unique ID for each mode
+    const uuid = this.api.hap.uuid.generate(uniqueId);
+    const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+
+    if (existingAccessory) {
+      this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+      existingAccessory.context.device = device;
+      existingAccessory.context.mode = mode; // Store the mode in the context
+      new HeatzyAccessory(this, existingAccessory, device, mode);
+    } else {
+      const displayName = `${device.dev_alias} ${mode}`;
+      this.log.info('Adding new accessory:', displayName);
+
+      const accessory = new this.api.platformAccessory(displayName, uuid);
+      accessory.context.device = device;
+      accessory.context.mode = mode; // Store the mode in the context
+      new HeatzyAccessory(this, accessory, device, mode);
+      this.api.registerPlatformAccessories('homebridge-heatzy-pilote-platform', 'Heatzy', [accessory]);
+      this.accessories.push(accessory);
+    }
+  }
+
+  configureAccessory(accessory: PlatformAccessory): void {
+    this.log.info('Configuring accessory:', accessory.displayName);
     this.accessories.push(accessory);
   }
 
-  /**
-   * This is an example method showing how to register discovered accessories.
-   * Accessories must only be registered once, previously created accessories
-   * must not be registered again to prevent "duplicate UUID" errors.
-   */
-  discoverDevices() {
+  getToken(): string | null {
+    return this.token;
+  }
 
-    // EXAMPLE ONLY
-    // A real plugin you would discover accessories from the local network, cloud services
-    // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-    ];
-
-    // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
-
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
-
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
-      const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
-
-      if (existingAccessory) {
-        // the accessory already exists
-        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-
-        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
-
-        // create the accessory handler for the restored accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, existingAccessory);
-
-        // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
-        // remove platform accessories when no longer present
-        // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-        // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
-      } else {
-        // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
-
-        // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
-
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = device;
-
-        // create the accessory handler for the newly create accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
-
-        // link the accessory to your platform
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-      }
-    }
+  needsAuthentication(): boolean {
+    return !this.token || !this.tokenExpireAt || this.tokenExpireAt < Date.now();
   }
 }
--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,141 +1,139 @@
-import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
+import { PlatformAccessory, Service, Characteristic, CharacteristicValue } from 'homebridge';
+import axios from 'axios';
+import { Heatzy } from './platform';
 
-import { ExampleHomebridgePlatform } from './platform';
-
-/**
- * Platform Accessory
- * An instance of this class is created for each accessory your platform registers
- * Each accessory may expose multiple services of different service types.
- */
-export class ExamplePlatformAccessory {
+export class HeatzyAccessory {
   private service: Service;
-
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
+  private modeMapping = {
+    'Confort': 0,
+    'Eco': 4,
+    'Eco Plus': 5,
+    'Sleep': 1,
+    'Antifreeze': 2,
   };
 
+  private reverseModeMapping = {
+    'cft': 'Confort',
+    'eco': 'Sleep',
+    'fro': 'Antifreeze',
+    'stop': 'Off',
+    'cft1': 'Eco',
+    'cft2': 'Eco Plus',
+  };
+
+  private off_mode = 3;
+  private mode: string; // Add mode as a class property
+
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: Heatzy,
     private readonly accessory: PlatformAccessory,
+    private readonly device: any,
+    mode: string, // Constructor parameter
   ) {
+    this.mode = mode; // Store mode
+    this.platform.log.info('Initializing accessory:', accessory.displayName);
 
-    // set accessory information
-    this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+    this.service = this.accessory.getService(this.platform.api.hap.Service.Switch) ||
+                   this.accessory.addService(this.platform.api.hap.Service.Switch, accessory.displayName);
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
-
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
-
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
-
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
-
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
-
-    /**
-     * Creating multiple services of the same type.
-     *
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     *
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
-
-    // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
-
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
-
-    /**
-     * Updating characteristics values asynchronously.
-     *
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     *
-     */
-    let motionDetected = false;
-    setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
-
-      // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
-
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
+    this.service.getCharacteristic(this.platform.api.hap.Characteristic.On)
+      .on('set', (value, callback) => this.setDeviceState(!!value, callback))
+      .on('get', callback => this.getDeviceState(callback));
+    this.startPolling();
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
-  async setOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
-
-    this.platform.log.debug('Set Characteristic On ->', value);
+  // Add a method to fetch device status periodically
+  startPolling() {
+    // Fetch the device status initially
+    this.getDeviceState((error, isOn) => {
+      if (!error) {
+        // Schedule to fetch device status every 60 seconds (60,000 milliseconds)
+        setInterval(() => {
+          this.getDeviceState((error, isOn) => {
+            if (error) {
+              this.platform.log.error('Failed to fetch device state:', error);
+            }
+          });
+        }, 60000); // 60 seconds
+      } else {
+        this.platform.log.error('Failed to fetch initial device state:', error);
+      }
+    });
   }
 
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   *
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   *
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
+  async setDeviceState(value: boolean, callback: Function) {
+    if (this.platform.needsAuthentication()) {
+      await this.platform.authenticate();
+    }
 
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
-  async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
+    const modeToSet = value ? this.modeMapping[this.mode] : this.off_mode;
+    const url = `https://euapi.gizwits.com/app/control/${this.device.did}`;
+    const payload = { attrs: { mode: modeToSet } };
 
-    this.platform.log.debug('Get Characteristic On ->', isOn);
+    try {
+      const response = await axios.post(url, payload, {
+        headers: {
+          'X-Gizwits-Application-Id': 'c70a66ff039d41b4a220e198b0fcc8b3',
+          'X-Gizwits-User-token': this.platform.getToken(),
+        },
+      });
 
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-
-    return isOn;
+      if (response.status === 200) {
+        // Log the success message with device name, mode, and state
+        const stateStr = value ? 'On' : 'Off';
+        this.platform.log.info(`Device state set successfully: ${this.accessory.displayName} - Mode: ${this.mode} - State: ${stateStr}`);
+        callback(null); // No error
+      } else {
+        throw new Error(`Request failed with status code ${response.status}`);
+      }
+    } catch (error) {
+      this.platform.log.error('Failed to set device state:', (error as Error).message);
+      callback(error); // Error
+    }
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
-  async setBrightness(value: CharacteristicValue) {
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
+  async getDeviceState(callback: Function) {
+    if (this.platform.needsAuthentication()) {
+      await this.platform.authenticate();
+    }
 
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
+    const url = `https://euapi.gizwits.com/app/devdata/${this.device.did}/latest`;
+
+    try {
+      const response = await axios.get(url, {
+        headers: {
+          'Accept': 'application/json',
+          'X-Gizwits-User-token': this.platform.getToken(),
+          'X-Gizwits-Application-Id': 'c70a66ff039d41b4a220e198b0fcc8b3',
+        },
+      });
+
+      // Log the status code and response data for debugging
+      // this.platform.log.debug('Response status:', response.status);
+      // this.platform.log.debug('Response data:', response.data);
+
+      if (response.status === 200 && response.data && response.data.attr) {
+        const apiMode = response.data.attr.mode;
+        const currentMode = this.reverseModeMapping[apiMode] || 'Unknown'; // Default to 'Unknown' if not found
+        const isOn = currentMode === this.mode; // Compare with human-readable mode
+
+        if (currentMode === 'Unknown') {
+          // Log the mode received from the API
+          this.platform.log.warn(`Unknown mode received from API: ${apiMode}`);
+        }
+        this.platform.log.info(`Fetched device state: ${this.accessory.displayName} - Mode: ${currentMode} - State: ${isOn ? 'On' : 'Off'}`);
+        callback(null, isOn);
+      } else {
+        throw new Error('Non-200 response or invalid data format');
+      }
+    } catch (error) {
+      this.platform.log.error('Failed to get device state:', (error as Error).message);
+      if (axios.isAxiosError(error) && error.response) {
+        // Log the detailed response for more insight
+        // this.platform.log.error('Error response:', error.response.data);
+      }
+      callback(error);
+    }
   }
 
 }
