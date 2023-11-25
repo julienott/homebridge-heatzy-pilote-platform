269fa77d5a3dc3301b6dc6bd2ea7a038b99f5e43
Julien Ott <julien.ott@gmail.com>
2023-11-25 10:08:24 +0100

    working copy

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -37,29 +37,11 @@ export class HeatzyAccessory {
                    this.accessory.addService(this.platform.api.hap.Service.Switch, accessory.displayName);
 
                    this.service.getCharacteristic(this.platform.api.hap.Characteristic.On)
-                   .on('set', (value, callback) => this.setDeviceState(!!value, callback))
-                   .on('get', (callback) => {
-                     this.getDeviceState()
-                       .then(isOn => callback(null, isOn))
-                       .catch(error => {
-                         this.platform.log.error('Error getting device state:', error);
-                         callback(error); // Pass the error to the callback
-                       });
-                   });
-    this.updateDeviceState();
+                   .on('set', (value, callback) => this.setOnCharacteristicHandler(value, callback))
+                   .on('get', callback => this.getOnCharacteristicHandler(callback));
   }
 
-  async updateDeviceState() {
-    try {
-      const isOn = await this.getDeviceState();
-      this.service.updateCharacteristic(this.platform.api.hap.Characteristic.On, isOn);
-    } catch (error) {
-      this.platform.log.error('Error updating device state:', error);
-    }
-    setTimeout(() => this.updateDeviceState(), 60000); // 60 seconds
-  }
-
-  async setDeviceState(value: boolean, callback: Function) {
+  async setOnCharacteristicHandler(value: CharacteristicValue, callback: Function) {
     if (this.platform.needsAuthentication()) {
       await this.platform.authenticate();
     }
@@ -69,57 +51,49 @@ export class HeatzyAccessory {
     const payload = { attrs: { mode: modeToSet } };
 
     try {
-      const response = await axios.post(url, payload, {
+      await axios.post(url, payload, {
         headers: {
           'X-Gizwits-Application-Id': 'c70a66ff039d41b4a220e198b0fcc8b3',
           'X-Gizwits-User-token': this.platform.getToken(),
         },
       });
 
-      if (response.status === 200) {
-        const stateStr = value ? 'On' : 'Off';
-        this.platform.log.info(`Device state set successfully: ${this.accessory.displayName} - Mode: ${this.mode} - State: ${stateStr}`);
-        callback(null); // No error
-      } else {
-        throw new Error(`Request failed with status code ${response.status}`);
-      }
+      this.platform.log.info(`Device state set to: ${value ? 'On' : 'Off'}`);
+      callback(null); // No error
     } catch (error) {
-      this.platform.log.error('Failed to set device state:', (error as Error).message);
-      callback(error); // Error
+      this.platform.log.error('Failed to set device state:', error);
+      callback(error); // Pass error to callback
     }
-}
-
-
-async getDeviceState(): Promise<boolean> {
-  if (this.platform.needsAuthentication()) {
-      await this.platform.authenticate();
   }
 
-  const url = `https://euapi.gizwits.com/app/devdata/${this.device.did}/latest`;
+  async getOnCharacteristicHandler(callback: Function) {
+    if (this.platform.needsAuthentication()) {
+      await this.platform.authenticate();
+    }
 
-  try {
+    const url = `https://euapi.gizwits.com/app/devdata/${this.device.did}/latest`;
+
+    try {
       const response = await axios.get(url, {
-          headers: {
-              'Accept': 'application/json',
-              'X-Gizwits-User-token': this.platform.getToken(),
-              'X-Gizwits-Application-Id': 'c70a66ff039d41b4a220e198b0fcc8b3',
-          },
+        headers: {
+          'Accept': 'application/json',
+          'X-Gizwits-User-token': this.platform.getToken(),
+          'X-Gizwits-Application-Id': 'c70a66ff039d41b4a220e198b0fcc8b3',
+        },
       });
 
       if (response.status === 200 && response.data && response.data.attr) {
-          const apiMode = response.data.attr.mode;
-          const currentMode = this.reverseModeMapping[apiMode] || 'Unknown'; 
-          const isOn = currentMode === this.mode;
+        const apiMode = response.data.attr.mode;
+        const currentMode = this.reverseModeMapping[apiMode] || 'Unknown'; 
+        const isOn = currentMode === this.mode;
 
-          return isOn;
+        callback(null, isOn);
       } else {
-          throw new Error('Non-200 response or invalid data format');
+        throw new Error('Non-200 response or invalid data format');
       }
-  } catch (error) {
+    } catch (error) {
       this.platform.log.error('Failed to get device state:', error);
-      throw error;
+      callback(error);
+    }
   }
-}
-
-
 }
