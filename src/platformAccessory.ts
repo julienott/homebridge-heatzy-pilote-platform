d3c6dd3210917096015fee28d8d9a7b5be4d7246
Julien Ott <julien.ott@gmail.com>
2023-11-25 14:03:36 +0100

    feat: polling

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -39,6 +39,8 @@ export class HeatzyAccessory {
     this.service.getCharacteristic(this.platform.api.hap.Characteristic.On)
       .on('set', (value, callback) => this.setOnCharacteristicHandler(value, callback))
       .on('get', callback => this.getOnCharacteristicHandler(callback));
+
+    this.startPolling();
   }
 
   async setOnCharacteristicHandler(value: CharacteristicValue, callback: Function) {
@@ -58,7 +60,7 @@ export class HeatzyAccessory {
         },
       });
 
-      this.platform.log.info(`Device state for '${this.accessory.displayName}' set to: ${value ? '\x1b[32mOn\x1b[0m' : '\x1b[31mOff\x1b[0m'}`);
+      this.platform.log.info(`Device state for '${this.accessory.displayName}' set to: ${value ? 'On' : 'Off'}`);
       callback(null); // No error
     } catch (error) {
       this.platform.log.error('Failed to set device state:', error);
@@ -89,7 +91,7 @@ export class HeatzyAccessory {
         const currentMode = this.reverseModeMapping[apiMode] || 'Unknown';
         const isOn = currentMode === this.mode;
 
-        this.platform.log.info(`Current state of '${this.accessory.displayName}' is: ${isOn ? '\x1b[32mOn\x1b[0m' : '\x1b[31mOff\x1b[0m'}`);
+        this.platform.log.info(`Current state of '${this.accessory.displayName}' is: ${isOn ? 'On' : 'Off'}`);
         callback(null, isOn);
       } else {
         throw new Error('Non-200 response or invalid data format');
@@ -100,4 +102,44 @@ export class HeatzyAccessory {
     }
   }
 
+  async getDeviceState(): Promise<boolean> {
+    if (this.platform.needsAuthentication()) {
+      await this.platform.authenticate();
+    }
+
+    const url = `https://euapi.gizwits.com/app/devdata/${this.device.did}/latest`;
+
+    try {
+      const response = await axios.get(url, {
+        headers: {
+          'Accept': 'application/json',
+          'X-Gizwits-User-token': this.platform.getToken(),
+          'X-Gizwits-Application-Id': 'c70a66ff039d41b4a220e198b0fcc8b3',
+        },
+      });
+
+      if (response.status === 200 && response.data && response.data.attr) {
+        const apiMode = response.data.attr.mode;
+        const currentMode = this.reverseModeMapping[apiMode] || 'Unknown';
+        return currentMode === this.mode;
+      } else {
+        throw new Error('Non-200 response or invalid data format');
+      }
+    } catch (error) {
+      this.platform.log.error(`Error polling device state for '${this.accessory.displayName}':`, error);
+      return false;
+    }
+  }
+
+  startPolling() {
+    const pollingInterval = 30000; // Poll every 30 seconds, adjust as necessary
+
+    const poll = async () => {
+      const isOn = await this.getDeviceState();
+      this.service.updateCharacteristic(this.platform.api.hap.Characteristic.On, isOn);
+    };
+
+    poll(); // Initial poll
+    setInterval(poll, pollingInterval); // Continuous polling
+  }
 }
